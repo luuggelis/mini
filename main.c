2396f765ef5608f3995c59a1213e58b524c7f7d5
smalltown <skunga2kl@gmail.com>
2026-09-25 20:55:55 +0300

    make a loop

--- main.c
+++ main.c
@@ -4,10 +4,13 @@ int main()
 {
     char input[256];
 
-    printf("mini> ");
-    fgets(input, sizeof(input), stdin);
+    while (1)
+    {        
+        printf("mini> ");
+        fgets(input, sizeof(input), stdin);
 
-    printf("you entered: %s", input);
+        printf("you entered: %s", input);
+    }
 
     return 0;
 }
